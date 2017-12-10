b1cc8301371e0bbc46e89935f330e238ee4bcefd
Tranced <terhyang@gmail.com>
2017-12-10 02:19:34 -0800

    removed test from package.json

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const slackBots = require('slackbots');
+const SlackBot = require('slackbots');
+
+
 
 let bot = new SlackBot({
-	slackToken: process.env.TOKEN,
+	token: process.env.TOKEN,
 	weatherAPIKey: process.env.API_KEY,
 	name : "weather-bot"
 });
 
-bot.on('start', function())
-{
+bot.on('start', function(){
 	var params = {
         icon_emoji: ":sunglasses:"
     };
 
 	bot.postMessageToChannel("general", 
-		"Hi, I'm weather-bot! To ask me about the weather, please type ```!weather city-name``` or ```!weather city-name,country```");
+		"Hi, I'm weather-bot! To ask me about the weather, please send me a message with the format: `!weather city-name``or `!weather city-name,country`", params);
 
-}
+});
 
-bot.on('message', function(data)){
+bot.on('message', function(data){
 	console.log(data);
-}
+});
