b36bd28e8f60d77af2b960e2a807ba1b09d0ef98
Tranced <terhyang@gmail.com>
2017-12-10 04:58:57 -0800

    Added main weather logic

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const SlackBot = require('slackbots');
+const request = require('request');
 
-const PORT = process.env.PORT || 5000;
+const weatherAPIKey = process.env.API_KEY;
 
+//make new instance of slackbot
 let bot = new SlackBot({
-	PORT: PORT,
 	token: process.env.TOKEN,
-	weatherAPIKey: process.env.API_KEY,
 	name : "weather-bot"
 });
 
+
+//initialize slackbot
 bot.on('start', function(){
-	var params = {
+	let params = {
         icon_emoji: ":sunglasses:"
     };
 
@@ -19,10 +21,60 @@ bot.on('start', function(){
 
 });
 
+
+//helper function for different weather cases using weather[0].id
+//probably could replace with object literal
+let weatherSwitch = function weatherSwitch(id){
+	//look at first number in code for common weather categories
+	switch(id[0]){
+		case "2":
+			return ":thunder_cloud_and_rain:";
+		case "3":
+		case "5":
+			return ":rain_cloud:";
+		case "6":
+			return ":snow_cloud:";
+		case "7":
+			if(id == "781"){
+				return ":tornado:";
+			} else{
+				return ":fog:";
+			}
+		case "8":
+			if(id == "800"){
+				return ":sunny:";
+			}else{
+				return ":partly_sunny:";
+			}
+
+	}
+	if(id < 903){
+		return ":tornado:";
+	} 
+	return ":exlamation:";
+}
+
 bot.on('message', function(data){
-	console.log(data);
-
-	let args = data.text;
-
-	//if(args.substring(0,1) == "!")
+	let user = data.user;
+	//check for exclamation mark in case
+	//future commands are to be added
+	if(data.text.substring(0,1) == "!"){
+		let cmd = data.text.splice(1).split(" ", 1);
+		let cmdStart = data.text.indexOf(" ");
+		switch(cmd){
+			case "weather":
+				let args = data.text.slice(cmdStart+1)
+				let url = `http://api.openweathermap.org/data/2.5/weather?q=${args}&APPID=${weatherAPIKey}`
+				request(url,function(error, response, body){
+		        	if(error){
+		        		bot.postMessageToUser(user,error,params);
+		        	} else{
+		        		let weather = JSON.parse(body);
+		        		let fahrenheit = 9/5*(weather.main.temp-273.15) + 32;
+		        		bot.postMessageToUser(user, weather.weather[0].main + " " + weatherSwitch(weather.weather[0].id, params);
+		        		bot.postMessageToUser(user, "It's " + fahrenheit +" degrees Fahrenheit in " + weather.name, params);
+		        	}
+	        	})
+		}
+	}
 });
